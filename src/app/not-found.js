b930e20672b1287c0e93608391c640fa2b736efa
ayushsaini111 <youremail@example.com>
2025-08-24 17:42:33 +0530

    update1.8

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,9 @@
 // app/not-found.js
-"use client";
-
 export default function NotFoundPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-white">
-      <h1 className="text-4xl font-bold mb-2">404 - Page Not Found</h1>
-      <p className="text-gray-400">The page you are looking for doesn’t exist.</p>
+      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+      <p className="text-gray-400">The page you are looking for does not exist.</p>
     </div>
   );
 }
